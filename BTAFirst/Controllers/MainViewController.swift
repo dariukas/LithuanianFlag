3fd58d319f8eb460ced118901b611dcec4915590
dmiliauskas <darius@voicehit.com>
2017-04-04 20:43:32 +0300

    print Commit

--- BTAFirst/Controllers/MainViewController.swift
+++ BTAFirst/Controllers/MainViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Labas")
+//        print("Labas")
     }
 
     override func didReceiveMemoryWarning() {
