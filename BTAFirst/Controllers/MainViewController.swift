4bf9ea613620c745955de5fba907027edbdc8756
dmiliauskas <darius@voicehit.com>
2017-04-04 20:36:24 +0300

    Initial Commit

---
+++ BTAFirst/Controllers/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BTAFirst
+//
+//  Created by Darius Miliauskas on 2017-04-04.
+//  Copyright © 2017 Darius Miliauskas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        print("Labas")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+//         Dispose of any resources that can be recreated.
+    }
+
+
+}
+
